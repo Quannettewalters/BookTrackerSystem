81b1c72aa4d9d07624273564fa9c06743eb4f08d
Quannette Walters <QW@iMac.attlocal.net>
2025-07-08 01:22:13 -0400

    method to display unique books added

--- src/model/LibraryManager.java
+++ src/model/LibraryManager.java
@@ -1,15 +1,13 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class LibraryManager {
     Set<String> userIdSet = new HashSet<>();
     List<User> users = new ArrayList<>();
     Set<String> bookTitleSet = new HashSet<>();
     List<Book> books = new ArrayList<>();
+    Set<String> uniqueBookTitles = new HashSet<>()
 
     //method to add user
     public void addUser(User user){
@@ -36,8 +34,56 @@ public class LibraryManager {
     public void recordFeedback (String userId, String bookId, String feedback){
         for (User user : users) {
             if (user.getUserId().equals(userId)) {
-                int wordCount = feedback.trim().split()
+                int wordCount = feedback.trim().split("\\s+").length;
+                if (wordCount >= 3) {
+                    user.addFeedback(bookId, feedback);
+                    System.out.println("Feedback recorded for" + user.getName());
+                }else {
+                    System.out.println("Feedback isn't long enough");
+                }
+                return;
             }
+        }
+        System.out.println("User not found");
+    }
+
+    //method to display all books borrowed by a user
+    public void displayAllBooksBorrowed(String userId) {
+        for (User user : users) {
+            if (userId.equals(user.getUserId())){
+                System.out.println("Books borrowed by: " + user.getName());
+                for (String bookId : user.getBorrowedBooksId()){
+                    System.out.println("Book Id: " + bookId);
+                    for (Book book : books) {
+                        if (book.getBookId().equals(bookId)) {
+                            System.out.println("Book Title: " + book.getTitle());
+                        }
+                    }
+                }
+                return;
+            }
+        }
+        System.out.println("User not found");
+    }
+
+    //method to analyze the existing feedbacks
+    public void analyzeFeedback() {
+        for (User user : users){
+            Map<String, String> usersFeedback = user.getFeedback();
+            for (String feedback : usersFeedback.values()) {
+                String fb = feedback.toLowerCase();
+                int wordCount = fb.trim().split("\\s+").length;
+                if (fb.contains("Good") || fb.contains("Excellent")){
+                    System.out.println("Feedback by this user " + user.getName() + " has commented positive.");
+                    System.out.println("This person's feedback has " + wordCount + " words.");
+                }
+            }
+        }
+    }
+
+    public void showAllUniqueBookTitles() {
+        for (User user : users){
+
         }
     }
 }
